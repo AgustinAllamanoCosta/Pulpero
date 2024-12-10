2d7e623bd578ecce398ead7305c4ac667d587c6d
AgustinaAllamanoCosta <allamanocostaagustin@gmail.com>
2024-12-10 14:13:36 -0300

    fix: 🐛 model download path for windows OS

--- lua/pulpero/core/setup.lua
+++ lua/pulpero/core/setup.lua
@@ -109,7 +109,7 @@ function Setup.create_directory(self, path)
         self:execute_command_and_dump('mkdir -p "' .. path .. '"')
     end
 end
-
+:
 function Setup.ensure_dir(self, path)
     self.logger:setup("Cheking if file exist " .. path)
     if self:is_directory(path) then
@@ -121,7 +121,13 @@ function Setup.download_model(self)
     self.logger:setup("Prepearing model")
     local data_dir = self:get_data_path()
     self:ensure_dir(data_dir)
-    local model_path = data_dir .. '/model.gguf'
+    local model_path = ''
+    local platform = self:get_platform()
+    if platform == 'windows' then
+        model_path =  data_dir .. '\\model.gguf'
+    else
+        model_path =  data_dir .. '/model.gguf'
+    end
     if self:file_exist(model_path) then
         self.logger:setup("Model already exist skiping download")
         return model_path, 0
