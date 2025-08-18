9bf42b0ca55f894493743f8118103a93e3261f1f
AgustinaAllamanoCosta <allamanocostaagustin@gmail.com>
2025-08-18 00:58:10 -0300

    refactor: 💡 Routes and pipelines

--- lua/pulpero/core/router/router.lua
+++ lua/pulpero/core/router/router.lua
@@ -36,102 +36,95 @@ function Router:route(user_message, file_context_data)
     -- Add some basic security check of prompt leaking
     local intention = self:detect_intention(user_message):gsub("%s+", ""):gsub("\n", "")
     local response = "No pipeline category found"
+    local file_context_data_str = string.format(
+        "Current working dir: %s\nOpen file name: %s\nOpen file dir path: %s\n",
+        self.file_context_data.current_working_dir,
+        self.file_context_data.current_file_name,
+        self.file_context_data.current_file_path
+    )
 
     if intention == "file_operations" then
-        response = self:file_pipeline(user_message)
+        response = self:file_pipeline(user_message, file_context_data_str)
     elseif intention == "code_analysis" then
-        response = self:code_analysis_pipeline(user_message)
+        response = self:code_analysis_pipeline(user_message, file_context_data_str)
     elseif intention == "general_chat" then
-        response = self:general_chat_pipeline(user_message)
+        response = self:general_chat_pipeline(user_message, file_context_data_str)
     end
 
     return response
 end
 
 function Router:detect_intention(user_message)
-    local complete_prompt = string.format(prompts.intent_prompt, self.history:generate_chat_history(), user_message)
+    local chat_history = self.history:generate_chat_history()
+    local complete_prompt = string.format(prompts.intent_prompt, chat_history, user_message)
     local prompt_file = prompts:generate_prompt_file(complete_prompt)
     local model_response = self.model_runner:talk_with_model(prompt_file)
     os.remove(prompt_file)
     return model_response
 end
 
-function Router:file_pipeline(user_message)
-    local file_context_data_str = string.format(
-        "Current working dir: %s\n Open file name: %s\n Open file dir path: %s\n",
-        self.file_context_data.current_working_dir,
-        self.file_context_data.current_file_name,
-        self.file_context_data.current_file_path
-    )
-
+function Router:file_pipeline(user_message, file_context_data_str)
+    local chat_history = self.history:generate_chat_history()
+    local tool_description = self.tool_manager:generate_tools_description()
     local full_prompt = string.format(
         prompts.file_operation,
         file_context_data_str,
-        self.tool_manager:generate_tools_description(),
-        self.history:generate_chat_history(),
+        tool_description,
+        chat_history,
         user_message
     )
-    local prompts_file = prompts:generate_prompt_file(full_prompt)
-    local model_response_with_tool_call = self.model_runner:talk_with_model(prompts_file)
+    local prompt_file = prompts:generate_prompt_file(full_prompt)
+    local model_response_with_tool_call = self.model_runner:talk_with_model(prompt_file)
 
     local tool_response = self.tool_manager:execute_tool_if_exist_call(model_response_with_tool_call)
     self.history:update_chat_context_as_user(user_message)
 
+    local final_response = self:generate_final_response(user_message, tool_response)
+    os.remove(prompt_file)
+    return final_response
+end
+
+function Router:code_analysis_pipeline(user_message, file_context_data_str)
+    local chat_history = self.history:generate_chat_history()
+    local tool_description = self.tool_manager:generate_tools_description()
+    local full_prompt = string.format(
+        prompts.code,
+        file_context_data_str,
+        tool_description,
+        chat_history,
+        user_message
+    )
+    local prompt_file = prompts:generate_prompt_file(full_prompt)
+    local model_response_with_tool_call = self.model_runner:talk_with_model(prompt_file)
+
+    local tool_response = self.tool_manager:execute_tool_if_exist_call(model_response_with_tool_call)
+
+    self.history:update_chat_context_as_user(user_message)
+
+    local final_response = self:generate_final_response(user_message, tool_response)
+    os.remove(prompt_file)
+    return final_response
+end
+
+function Router:general_chat_pipeline(user_message, file_context_data_str)
+    local chat_history = self.history:generate_chat_history()
+    local full_prompt = string.format(prompts.chat, file_context_data_str, chat_history, user_message)
+    local prompt_file = prompts:generate_prompt_file(full_prompt)
+
+    self.history:update_chat_context_as_user(user_message)
+
+    local final_response = self.model_runner:talk_with_model(prompt_file)
+    self.history:update_chat_context_as_assistant(final_response)
+
+    os.remove(prompt_file)
+    return final_response
+end
+
+function Router:generate_final_response(user_message, tool_response)
     local final_response_prompt = string.format(prompts.generate_final_response, user_message, tool_response)
     local final_prompts_file = prompts:generate_prompt_file(final_response_prompt)
     local final_response = self.model_runner:talk_with_model(final_prompts_file)
     self.history:update_chat_context_as_assistant(final_response)
-
-    return final_response
-end
-
-function Router:code_analysis_pipeline(user_message)
-    local file_context_data_str = string.format(
-        "Current working dir: %s\n Open file name: %s\n Open file dir path: %s\n",
-        self.file_context_data.current_working_dir,
-        self.file_context_data.current_file_name,
-        self.file_context_data.current_file_path
-    )
-
-    local final_response = ""
-    local full_prompt = string.format(
-        prompts.code,
-        file_context_data_str,
-        self.tool_manager:generate_tools_description(),
-        self.history:generate_chat_history(),
-        user_message
-    )
-    local prompts_file = prompts:generate_prompt_file(full_prompt)
-    local model_response_with_tool_call = self.model_runner:talk_with_model(prompts_file)
-
-    local tool_response = self.tool_manager:execute_tool_if_exist_call(model_response_with_tool_call)
-
-    self.history:update_chat_context_as_user(user_message)
-
-    local final_response_prompt = string.format(prompts.generate_final_response, user_message, tool_response)
-    local final_prompts_file = prompts:generate_prompt_file(final_response_prompt)
-    final_response = self.model_runner:talk_with_model(final_prompts_file)
-    self.history:update_chat_context_as_assistant(final_response)
-
-    return final_response
-end
-
-function Router:general_chat_pipeline(user_message)
-    local file_context_data_str = string.format(
-        "Current working dir: %s\n Open file name: %s\n Open file dir path: %s\n",
-        self.file_context_data.current_working_dir,
-        self.file_context_data.current_file_name,
-        self.file_context_data.current_file_path
-    )
-
-    local chat_history = self.history:generate_chat_history()
-    local full_prompt = string.format(prompts.chat, file_context_data_str, chat_history, user_message)
-    local prompts_file = prompts:generate_prompt_file(full_prompt)
-
-    self.history:update_chat_context_as_user(user_message)
-    local final_response = self.model_runner:talk_with_model(prompts_file)
-    self.history:update_chat_context_as_assistant(final_response)
-
     return final_response
 end
 
